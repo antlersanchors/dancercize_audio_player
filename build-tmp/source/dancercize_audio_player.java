e7191fcf930a6545f8d87f7752cc1cbf53eb9abd
Michael Liston <michaelowen.liston@gmail.com>
2015-04-24 01:03:46 +0200

    Go-go minim AudioPlayer arraylist()

--- build-tmp/source/dancercize_audio_player.java
+++ build-tmp/source/dancercize_audio_player.java
@@ -4,6 +4,7 @@ import processing.event.*;
 import processing.opengl.*; 
 
 import ddf.minim.*; 
+import ddf.minim.ugens.*; 
 
 import java.util.HashMap; 
 import java.util.ArrayList; 
@@ -17,14 +18,26 @@ import java.io.IOException;
 public class dancercize_audio_player extends PApplet {
 
 
+
+
 Minim minim;
+
+ArrayList<AudioPlayer> players = new ArrayList<AudioPlayer>();
+
 AudioPlayer waltz;
 AudioPlayer rumba;
 AudioPlayer samba;
 AudioPlayer swing;
 
+
 public void setup() {
   minim = new Minim(this);
+
+  players.add(waltz);
+  players.add(rumba);
+  players.add(samba);
+  players.add(swing);
+
   waltz = minim.loadFile("waltz.mp3", 2048);
   rumba = minim.loadFile("rumba.mp3", 2048);
   samba = minim.loadFile("samba.mp3", 2048);
@@ -34,27 +47,14 @@ public void setup() {
 
 public void draw() {
 	size(512, 512);
+
+	println(players.size());
 	
 }
 
 public void keyPressed() {
-	if (key == '1'){
-		minim.stop();
-		waltz.play();
-	}
-
-	if (key == '2'){
-		minim.stop();
-		rumba.play();
-	}
-
-	if (key == '3'){
-		samba.play();
-	}
-
-	if (key == '4'){
-		swing.play();
-	}
+	int k = key;
+ 
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "dancercize_audio_player" };
