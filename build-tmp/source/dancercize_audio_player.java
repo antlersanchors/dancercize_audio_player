95c08cf888c3c6a5744593bf7b4fed776e1188f9
Michael Liston <michaelowen.liston@gmail.com>
2015-04-24 14:13:34 +0200

    Revert cdc4243..fe06c34
    This rolls back to commit cdc424308913e42601379c5d18a25d7871b77964.

--- build-tmp/source/dancercize_audio_player.java
+++ build-tmp/source/dancercize_audio_player.java
@@ -4,7 +4,6 @@ import processing.event.*;
 import processing.opengl.*; 
 
 import ddf.minim.*; 
-import ddf.minim.ugens.*; 
 
 import java.util.HashMap; 
 import java.util.ArrayList; 
@@ -18,26 +17,14 @@ import java.io.IOException;
 public class dancercize_audio_player extends PApplet {
 
 
-
-
 Minim minim;
-
-ArrayList<AudioPlayer> players = new ArrayList<AudioPlayer>();
-
 AudioPlayer waltz;
 AudioPlayer rumba;
 AudioPlayer samba;
 AudioPlayer swing;
 
-
 public void setup() {
   minim = new Minim(this);
-
-  players.add(waltz);
-  players.add(rumba);
-  players.add(samba);
-  players.add(swing);
-
   waltz = minim.loadFile("waltz.mp3", 2048);
   rumba = minim.loadFile("rumba.mp3", 2048);
   samba = minim.loadFile("samba.mp3", 2048);
@@ -47,14 +34,27 @@ public void setup() {
 
 public void draw() {
 	size(512, 512);
-
-	println(players.size());
 	
 }
 
 public void keyPressed() {
-	int k = key;
- 
+	if (key == '1'){
+		minim.stop();
+		waltz.play();
+	}
+
+	if (key == '2'){
+		minim.stop();
+		rumba.play();
+	}
+
+	if (key == '3'){
+		samba.play();
+	}
+
+	if (key == '4'){
+		swing.play();
+	}
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "dancercize_audio_player" };
